7456bcf6d855bcc0e3dc9d663b99c3e4ad69e147
Richard Johnson <rjohnson2590@gmail.com>
2015-07-02 17:30:46 -0700

    Use backbone, would be good for levels, different views are differnet levels

--- portfolio/app.js
+++ portfolio/app.js
@@ -15,12 +15,60 @@ var canvas = document.getElementById("canvas"),
       speed: 3,
       velX: 0,
       velY: 0,
-      jumping: false
+      jumping: false,
+      grounded: false
     },
     keys = [],
     friction = 0.8;
     gravity = 0.2;
- 
+
+var blocks = [];
+
+blocks.push({
+  x:0,
+  y:0,
+  width: 10,
+  height: height
+});
+blocks.push({
+  x:0,
+  y: height - 2,
+  width: width,
+  height: 50
+});
+blocks.push({
+  x: width-10,
+  y:0,
+  width: 50,
+  height: height
+});
+// blocks.push({
+//     x: 120,
+//     y: 10,
+//     width: 80,
+//     height: 80
+// });
+blocks.push({
+    x: 170,
+    y: 50,
+    width: 80,
+    height: 80
+});
+blocks.push({
+    x: 320,
+    y: 100,
+    width: 80,
+    height: 80
+});
+blocks.push({
+    x: 270,
+    y: 150,
+    width: 40,
+    height: 40
+});
+
+
+
 canvas.width = width;
 canvas.height = height;
  
@@ -29,8 +77,9 @@ function update(){
   // check keys
     if (keys[38] || keys[32]) {
         // up arrow or space
-      if(!player.jumping){
+      if(!player.jumping && player.grounded){
        player.jumping = true;
+       player.grounded= false;
        player.velY = -player.speed*2;
       }
     }
@@ -51,27 +100,71 @@ function update(){
  
     player.velY += gravity;
  
-    player.x += player.velX;
-    player.y += player.velY;
- 
-    if (player.x >= width-player.width) {
-        player.x = width-player.width;
-    } else if (player.x <= 0) {         
-        player.x = 0;     
-    }    
-  
-    if(player.y >= height-player.height){
-        player.y = height - player.height;
-        player.jumping = false;
-    }
- 
+
+
   ctx.clearRect(0,0,width,height);
+  ctx.fillStyle= 'black';
+  ctx.beginPath();
+  player.grounded= false;
+  for (var i =0; i< blocks.length; i++){
+    ctx.rect(blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height)
+    var dir = colCheck(player, blocks[i]);
+    if (dir === "l" || dir === "r") {
+     player.velX = 0;
+     player.jumping = false;
+ } else if (dir === "b") {
+     player.grounded = true;
+     player.jumping = false;
+ } else if (dir === "t") {
+     player.velY *= -1;
+ }
+    
+  }
+  if(player.grounded){
+     player.velY = 0;
+}
+ 
+  player.x += player.velX;
+  player.y += player.velY;
+  ctx.fill();
   ctx.fillStyle = "red";
   ctx.fillRect(player.x, player.y, player.width, player.height);
  
   requestAnimationFrame(update);
 }
  
+function colCheck(shapeA, shapeB) {
+    // get the vectors to check against
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+        // add the half widths and half heights of the objects
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+        colDir = null
+ 
+    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {         // figures out on which side we are colliding (top, bottom, left, or right)        
+     var oX = hWidths - Math.abs(vX),             oY = hHeights - Math.abs(vY);         if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
+
 document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
 });
